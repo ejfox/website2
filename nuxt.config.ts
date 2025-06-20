bcb70e95b1cda285886bf02deaf90475c69f9c1d
EJ Fox <ejfox@ejfox.com>
2025-06-19 23:04:34 -0400

    fix: enable SSR for Docker deployment API routes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   // SSR configuration
-  ssr: false,
+  ssr: true,
 
   // Add compatibility date
   compatibilityDate: '2024-02-23',
