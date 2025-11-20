93d2c8003576dc36d48b4efc69445c7399d37788
EJ Fox <ejfox@ejfox.com>
2025-11-20 13:32:19 -0500

    refactor(gists): remove hover states
    - Remove hover:underline effects
    - Remove hover:text-zinc-300 color changes
    - Static text presentation per Tufte

--- pages/gists.vue
+++ pages/gists.vue
@@ -207,7 +207,7 @@ const toggleGist = (gistId: string) => {
             :href="gist.html_url"
             target="_blank"
             rel="noopener"
-            class="hover:underline hover:text-zinc-300 truncate"
+            class="  truncate"
           >
             {{ Object.values(gist.files)[0]?.filename || 'Untitled' }}
           </a>
